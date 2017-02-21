ba21bef8ddaf106180bb94f4410559d57557614e
Chawit Wiriyakun <chawitmild@gmail.com>
2017-02-21 16:17:23 +0700

    Update URLClear.js

--- URLClear.js
+++ URLClear.js
@@ -1,5 +1,7 @@
+/*
+by chawit wiriyakun
+*/
 function URLClear(inurl){
-	
 	var urlout = inurl.replace(/<\/?[^>]+(>|$)/g, "").replace(/&nbsp;/gi, "").replace(/window.location=/gi, "").replace(/"/gi, "").replace(/;/gi, "").replace(/ /gi, "");
 	return urlout;
 }
